43eb6b3867ce507eda3f08086a8ea111eb90adc0
fukky21 <r.fukky21@gmail.com>
2021-09-05 16:13:50 +0900

    First Commit

---
+++ api/src/index.ts
@@ -0,0 +1,52 @@
+import express from 'express';
+
+interface IUser {
+  id: number;
+  name: string;
+  email: string;
+}
+
+// 仮想DB
+const users: IUser[] = [];
+
+// 初期データ投入
+const user1: IUser = {
+  id: 1,
+  name: 'ココア',
+  email: 'cocoa@example.com',
+};
+const user2: IUser = {
+  id: 2,
+  name: 'チノ',
+  email: 'chino@example.com',
+};
+const user3: IUser = {
+  id: 3,
+  name: 'リゼ',
+  email: 'rize@example.com',
+};
+const user4: IUser = {
+  id: 4,
+  name: '千夜',
+  email: 'chiya@example.com',
+};
+const user5: IUser = {
+  id: 5,
+  name: 'シャロ',
+  email: 'syaro@example.com',
+}
+users.push(user1);
+users.push(user2);
+users.push(user3);
+users.push(user4);
+users.push(user5);
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/users', (_, res) => {
+  res.status(200).json({ 'users': users });
+});
+
+app.listen(3080, () => console.log('Server is running...'));
---
+++ web/pages/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <el-container>
+    <el-main>
+      <el-table :data="users" stripe style="width: 100%">
+        <el-table-column prop="id" label="ID" width="180"></el-table-column>
+        <el-table-column prop="name" label="Name" width="180"></el-table-column>
+        <el-table-column prop="email" label="Email"></el-table-column>
+      </el-table>
+    </el-main>
+  </el-container>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator';
+
+@Component
+export default class IndexPage extends Vue {
+  users = [
+    {
+      id: 1,
+      name: 'Tom',
+      email: 'tom@example.com'
+    },
+    {
+      id: 2,
+      name: 'Aki',
+      email: 'aki@example.com'
+    },
+    {
+      id: 3,
+      name: 'Keiko',
+      email: 'keiko@example.com'
+    }
+  ];
+}
+</script>
